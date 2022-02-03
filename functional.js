383bacfb28a157525852bfb64341797e58bccfa0
Dinne Kopelevich <dinne22@gmail.com>
2022-02-03 15:49:24 -0700

    Add palindrome test

---
+++ functional.js
@@ -0,0 +1,54 @@
+let states = ["Kansas", "Nebraska", "North Dakota", "South Dakota"];
+
+// Returns a URL-friendly version of a string.
+//   Example: "North Dakota" -> "north-dakota"
+function urlify(string) {
+  return string.toLowerCase().split(/\s+/).join("-");
+}
+
+// urls: Imperative version
+function imperativeUrls(elements) {
+  let urls = [];
+  elements.forEach(function(element) {
+    urls.push(urlify(element));
+  });
+  return urls;
+}
+console.log(imperativeUrls(states));
+
+// urls: Functional version
+function functionalUrls(elements) {
+    return elements.filter(element => element.split(/\s+/).length === 1);
+}
+console.log(functionalUrls(states));
+
+// singles: Imperative version
+function imperativeSingles(elements) {
+    let singles = [];
+    elements.forEach(function(element) {
+        if (element.split(/\s+/).length === 1) {
+          singles.push(element);
+        }
+      });
+      return singles;
+    }
+    console.log(imperativeSingles(states));
+
+    let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+    // sum: Imperative solutions
+
+    function imperativeSum(elements) {
+        let total = 0;
+        elements.forEach(function(n) {
+            total += n;
+        });
+        return total;
+    }
+    console.log(imperativeSum(numbers));
+
+// sum: Functional solution
+function functionalSum(elements) {
+    return elements.reduce((total, n) => { return total += n; });
+  }
+  console.log(functionalSum(numbers));
